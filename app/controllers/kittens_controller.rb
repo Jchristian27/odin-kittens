649d1b6cd6101613f9ba5371b26d579fa6191344
Jonathan Christian <christianj707@gmail.com>
2024-03-12 11:38:49 -0700

    forget about using git like an idiot and finish project

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,22 +1,61 @@
 class KittensController < ApplicationController
   def index
+    @kittens = Kitten.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render :json => @kittens }
+    end
   end
 
   def show
+    @kitten = Kitten.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render :json => @kitten }
+    end
   end
 
   def new
+    @kitten = Kitten.new
   end
 
   def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      flash[:success] = "Kitten successfully created!"
+      redirect_to @kitten
+    else
+      render 'new', status: :unprocessable_entity
+    end
   end
 
   def edit
+    @kitten = Kitten.find(params[:id])
   end
 
   def update
+    @kitten = Kitten.find(params[:id])
+
+    if @kitten.update(kitten_params)
+      flash[:success] = "Kitten successfully updated!"
+      redirect_to @kitten
+    else
+      render 'edit', status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+    flash[:success] = "Kitten successfully destroyed!"
+    redirect_to root_path
   end
+
+  private
+    def kitten_params
+      params.require(:kitten).permit(:id, :name, :age, :cuteness, :softness)
+    end
 end
